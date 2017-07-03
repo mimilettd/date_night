2f70cc7721e0de71dad7eca78fa8e7be296ee2b5
mimilettd <mimilettd@gmail.com>
2017-07-03 14:35:28 -0600

    test for inserting nodes and returns depth

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -6,5 +6,10 @@ require_relative '../lib/node'
 require 'pry'
 
 class BinarySearchTreeTest < Minitest::Test
-
+  def test_tree_adds_new_node_and_returns_depth
+    assert_equal(0, tree.insert(61, "Bill & Ted's Excellent Adventure"))
+    assert_equal(1, tree.insert(16, "Johnny English"))
+    assert_equal(1, tree.insert(92, "Sharknado 3"))
+    assert_equal(2, tree.insert(50, "Hannibal Buress: Animal Furnace"))
+  end
 end
